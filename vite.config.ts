9d65d10216b5beff737ac411e5eeed3cc84242f7
sol2508 <sol2508@users.noreply.github.com>
2026-07-07 20:38:46 +0900

    GitHub Pages 흰 화면 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/Suhang-Moa/",
+  build: {
+    rollupOptions: {
+      input: "src/main.tsx",
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        chunkFileNames: "assets/[name].js",
+        entryFileNames: "assets/app.js",
+      },
+    },
+  },
   plugins: [react()],
 });
